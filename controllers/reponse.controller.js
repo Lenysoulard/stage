1deb43071f7184db0500ee349d4f6c58002ad363
Lény <soulardleny@gmail.com>
2024-04-23 13:41:46 +0200

    fix: error fix

--- controllers/reponse.controller.js
+++ controllers/reponse.controller.js
@@ -2,8 +2,10 @@ import { createReponse as create } from "../services/reponse.service.js";
 
 export const createReponse = async (req, res) => {
     try {
+        console.log(req.body);
         const reponse = await create(req.body.id_personne, req.body.dilemmes);
-        res.status(201).json(reponse);
+        console.log(reponse);
+        res.status(201).json({reponse : reponse});
     } catch (err) {
         res.status(500).json({ message: err.message, body: req.body});
     }
