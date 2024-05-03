b5a2877371a50c1648225cab06223cf679517f5a
Lény <soulardleny@gmail.com>
2024-05-04 01:54:14 +0200

    fix: remove useless console.log

--- controllers/dilemme_defaut.controller.js
+++ controllers/dilemme_defaut.controller.js
@@ -1,4 +1,5 @@
 import { getDilemmeDefautById, getDilemmeDefauts } from "../services/dilemme_defaut.service.js";
+import { getModificationDilemme } from "../services/modification.service.js";
 
 export const getDilemmesDefaut = async (req, res) => {
     try {
@@ -23,7 +24,8 @@ export const afficherDilemmes = async (req, res) => {
         const dilemmesDefaut = await getDilemmeDefauts();
         const randomIndex = Math.floor(Math.random() * dilemmesDefaut.length);
         const dilemmeDefaut = dilemmesDefaut[randomIndex];
-        res.render("userForm.ejs", {title: "Formulaire de participation", dilemme: dilemmeDefaut});
+        const modificationDilemme = await getModificationDilemme();
+        res.render("userForm.ejs", {title: "Formulaire de participation", dilemme: dilemmeDefaut, modification: modificationDilemme});
     } catch (err) {
         res.status(500).json({ message: err.message });
     }
@@ -31,11 +33,9 @@ export const afficherDilemmes = async (req, res) => {
 
 export const getDilemmeDefautsExcludeIds = async (req, res) => {
     try {
-        const dilemmesDefaut = await getDilemmeDefauts();
+        const dilemmesDefaut = await getDilemmeDefauts();   
         const ids = req.params.ids.split('-');
-        console.log(ids);
         const dilemmesDefautFiltered = dilemmesDefaut.filter(dilemme => !ids.includes(dilemme.id.toString()));
-        console.log(dilemmesDefautFiltered);
         res.status(200).json(dilemmesDefautFiltered);
     } catch (err) {
         res.status(500).json({ message: err.message });
--- controllers/reponse.controller.js
+++ controllers/reponse.controller.js
@@ -2,9 +2,7 @@ import { createReponse as create } from "../services/reponse.service.js";
 
 export const createReponse = async (req, res) => {
     try {
-        console.log(req.body);
         const reponse = await create(req.body.id_personne, req.body.dilemmes);
-        console.log(reponse);
         res.status(201).json({reponse : reponse});
     } catch (err) {
         res.status(500).json({ message: err.message, body: req.body});
