a3f9a9ca1dfadfc38fbfa83adde93c2db15efa4b
Soulard Leny <Soulardleny@gmail.com>
2024-05-13 12:08:27 +0200

    fix: useless code deleted

--- services/reponse.service.js
+++ services/reponse.service.js
@@ -1,13 +1,12 @@
 import reponseModel from "../models/reponse.model.js";
-import { createReponse as create , getReponseById} from "../repositories/reponse.repository.js";
+import { createReponse as create} from "../repositories/reponse.repository.js";
 
 export const createReponse = async (id_personne, dilemmes) => {
     try {
-        
-        const id_reponse = await create(id_personne, JSON.stringify(dilemmes));
-        // const reponse = await getReponseById(id_reponse);
-        // return new reponseModel(reponse[0].id, reponse[0].id_personne, reponse[0].id_dilemme, reponse[0].id_dilemme_contexte, reponse[0].choix);
+        await create(id_personne, JSON.stringify(dilemmes));
     } catch (err) {
         throw new Error(err);
     }
 }
+
+
